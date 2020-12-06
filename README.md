765ffbd215a07ec5b66cfba9b7dd2f8e0d03ceff
C.T. Bell <charlestbell@gmail.com>
2020-12-05 19:57:57 -0500

    Added the rest of the quiz. Finished the High Score Screen. Updated Readme.

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Coding-Quiz-App
+#### Developer C.T. Bell
+
+# Coding Quiz
+
+A simple coding quiz that keeps track of high scores
+
+## How scoring works
+
+At the end of the quiz, your score will be your time left. Watch out! Incorrect answers incur a time penalty.
+
+# Link to Live Site
+
+[Live Site](https://charlestbell.github.io/Coding-Quiz-App/)
+
+# Features
+
+- Tells you if the question was correct or incorrect
+- High scores are saved along with your initials
+- High scores are sorted with highest first
+- Ability to clear high scores
+- Built with Bootstrap and jQuery
+
+# Upcoming Features
+
+- Multi-answer questions
+
+## Screenshots
+
+### Start
+
+![Start](./Assets/start.jpg?raw=true "Start")
+
+### Quiz
+
+![Quiz](./Assets/quiz.jpg?raw=true "Quiz")
+
+### Correct
+
+![Correct](./Assets/correct.jpg?raw=true "Correct")
+
+### Enter Initials
+
+![Enter Initials](./Assets/enter_initials.jpg?raw=true "Ask include Numbers?")
+
+### Highscores
+
+![Highscores](./Assets/highscores.jpg?raw=true "Password printed to screen")
--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
-  var score = 0;
+  var playerScore = 0;
   var currentQuestion = 0;
-  var gameOver = false;
-  var gameTime = 10;
+  var gameTime = 75;
+  var highScores = [];
+  var intervalId;
 
-  // A way to store all the questions and answers. Probably a series of objects or an array of objects.
+  //Stores the Quiz
   var questions = [
     {
-      questionText: "Ways to declare functions in Javascript DO NOT include:",
+      questionText: "Which of these is a function declaration in Javascript?",
       answers: [
         "1. const lanchMissiles = function(target, howMany){}",
         "2. let function amazingFunction(param){}",
@@ -18,30 +19,36 @@ $(document).ready(function () {
     },
     {
       questionText:
-        "To store an <input> string as a number, use the ____ method.",
+        "To store an <input>'s string as a number, use the ____ method.",
       answers: [
         "1. Floater",
         "2. Math.floor",
-        "3. parseInt(}",
+        "3. parseInt{}",
         "4. saveToNum()",
       ],
       isCorrect: 2,
     },
+    {
+      questionText: "Which statement about Javascript Objects is false?",
+      answers: [
+        "1. Can be put into local storage with JSON",
+        "2. Can be stored in Arrays",
+        "3. Can contain strings",
+        "4. Are called using myObject()",
+      ],
+      isCorrect: 3,
+    },
+    {
+      questionText: "JS Libraries Include:",
+      answers: ["1. SASS", "2. Materialize", "3. React", "4. JQuerySelector"],
+      isCorrect: 2,
+    },
   ];
-  //Load the first question to the page
-  $("#question").text(questions[0].questionText);
-  $("#answer1").text(questions[0].answers[0]);
-  $("#answer2").text(questions[0].answers[1]);
-  $("#answer3").text(questions[0].answers[2]);
-  $("#answer4").text(questions[0].answers[3]);
 
-  //(Version 3)When a button is clicked, highlight it so the user knows that it is selected
-
-  //A timer in the upper right corner
-  //If time runs out and game is not over, game over
+  //Timer
   var startTimer = function () {
-    var intervalId = setInterval(function () {
-      if (gameTime === 0) {
+    intervalId = setInterval(function () {
+      if (gameTime === 0 && $("#quiz").is(":visible")) {
         clearInterval(intervalId);
         endGame();
         return;
@@ -51,62 +58,122 @@ $(document).ready(function () {
     }, 1000);
   };
 
-  //A function that ends the game endGame()
+  //A function that hides the quiz and reveals the Get Initials screen
+  function endGame() {
+    clearInterval(intervalId);
+    playerScore = gameTime;
+    $("#initials").val("");
+    $("#highScore").text(playerScore);
+    $("nav span").hide();
+    $("#quiz").hide();
+    $("#getInitials").show();
+  }
 
-  // Check the array to see if the answers were correct
-  //If wrong, subtract seconds from timer. 5 seconds.
+  // Loop through the highscores object and create a line to display each stored player
+  function renderHighScores() {
+    highScores.sort(function (a, b) {
+      if (a.score < b.score) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+    $("#highScorePage ul").empty();
+    $.each(highScores, function (i, person) {
+      listItem = $("<div>");
+      listSpan = $("<span />");
+      listItem.addClass("row");
+      listSpan.addClass("badge-pill badge-primary mb-1");
+      listSpan.text(
+        `${i + 1}. ${person.initial.toUpperCase()} with ${person.score} points`
+      );
+      listItem.append(listSpan);
+      $("#highScorePage ul").append(listItem);
+    });
+  }
 
-  //If correct, Call the function that changes to the next question
-  $("#buttons").on("click", function (event) {
+  //Hide the alerts after 2 seconds
+  function hideAlert() {
+    setTimeout(function () {
+      $("#passFail").hide();
+    }, 2000);
+  }
+
+  // Switch to the next question
+  function nextQuestion() {
+    if (currentQuestion < questions.length) {
+      $("#question").text(questions[currentQuestion].questionText);
+      $("#answer1").text(questions[currentQuestion].answers[0]);
+      $("#answer2").text(questions[currentQuestion].answers[1]);
+      $("#answer3").text(questions[currentQuestion].answers[2]);
+      $("#answer4").text(questions[currentQuestion].answers[3]);
+    } else endGame();
+  }
+
+  //Check if question is correct. If wrong, subtract time.
+  $("#quiz").on("click", function (event) {
     if (event.target.matches("button")) {
       if (
         parseInt(event.target.dataset["answer"]) ===
         questions[currentQuestion].isCorrect
       ) {
-        console.log("Correct!!");
         currentQuestion++;
         $("#passFail p").text("Correct!");
-        $("#passFail").removeClass("hide");
-        // debugger;
-
-        passFailTimeout();
+        $("#passFail").show();
+        hideAlert();
         nextQuestion();
       } else {
-        gameTime = gameTime - 5;
+        gameTime = gameTime - 15;
         currentQuestion++;
         $("#passFail p").text("Wrong!");
-        $("#passFail").removeClass("hide");
-        // debugger;
-        passFailTimeout();
+        $("#passFail").show();
+        hideAlert();
         nextQuestion();
       }
     }
   });
 
-  // Display correct or incorrect for 3 seconds, then hide them.
-  //A timer function that displays the correct/incorrect div for 3 senconds
-  function passFailTimeout() {
-    setTimeout(function () {
-      $("#passFail").addClass("hide");
-    }, 2000);
-  }
-
-  // A Function that Replacse the question and answers with the next question based on the global var
-  function nextQuestion() {
-    $("#question").text(questions[currentQuestion].questionText);
-    $("#answer1").text(questions[currentQuestion].answers[0]);
-    $("#answer2").text(questions[currentQuestion].answers[1]);
-    $("#answer3").text(questions[currentQuestion].answers[2]);
-    $("#answer4").text(questions[currentQuestion].answers[3]);
-  }
-
-  //A function that starts the qu
+  //Start the quiz
   $("#startQuiz").on("click", function () {
-    console.log("click fired");
+    nextQuestion();
     $("#startScreen").hide();
     $("#quiz").show();
+    $("#timer").parent().show();
+    $("#timer").show();
+    startTimer();
+  });
+
+  //Go to high score screen after Get Initials screen
+  $("#getInitials button").on("click", function (event) {
+    event.preventDefault();
+    $("#getInitials").hide();
+    $("#highScorePage").show();
+    var person = {
+      initial: $("#initials").val(),
+      score: playerScore,
+    };
+    highScores.push(person);
+    renderHighScores();
+  });
+
+  //Clear the high scores
+  $("#clear").on("click", function () {
+    highScores = [];
+    renderHighScores();
+  });
+
+  //When the Go Back button is clicked, start the quiz over
+  $("#goBack").on("click", function () {
+    currentQuestion = 0;
+    gameTime = 75;
+    $("#highScorePage").hide();
+    $("#startScreen").show();
+  });
+
+  //Go to High Scores page when View High Scores button is clicked
+  $("#viewHighscores").on("click", function () {
+    $("#startScreen, #getInitials, #quiz").hide();
+    renderHighScores();
+    $("#highScorePage").show();
   });
 });
-//A function that changes the screen to the high score screen. By hiding the quiz and revealing the high score div.
-// Build a high score screen in html
-// Build a start menu screen in HTML.
